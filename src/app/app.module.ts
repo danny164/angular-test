dc2ea23fa2750e6402feaaf1339b73a9c5960fb5
DANNY <qnv164@gmail.com>
2022-04-01 12:26:20 +0700

    Add layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { BannerComponent } from './pages/home/components/banner/banner.component';
+import { TopCategoriesComponent } from './pages/home/components/top-categories/top-categories.component';
+import { ProductsComponent } from './pages/home/components/products/products.component';
+import { BranchLogosComponent } from './pages/home/components/branch-logos/branch-logos.component';
+import { NewsletterComponent } from './pages/home/components/newsletter/newsletter.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [AppComponent, HomeComponent, HeaderComponent, FooterComponent, BannerComponent, TopCategoriesComponent, ProductsComponent, BranchLogosComponent, NewsletterComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, AlertModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/pages/home/components/branch-logos/branch-logos.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-branch-logos',
+  templateUrl: './branch-logos.component.html',
+  styleUrls: ['./branch-logos.component.scss'],
+})
+export class BranchLogosComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
